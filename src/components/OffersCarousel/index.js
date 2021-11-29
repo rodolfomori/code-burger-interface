5b4dfbc2f0ef6ec18cf55027c78d8dcc6a1bb9c5
Rodolfo Mori <rod_dolpho@hotmail.com>
2021-11-29 06:35:06 -0300

    finished carousel

---
+++ src/components/OffersCarousel/index.js
@@ -0,0 +1,59 @@
+import React, { useEffect, useState } from 'react'
+import Carousel from 'react-elastic-carousel'
+
+import Offers from '../../assets/offers.png'
+import api from '../../services/api'
+import formatCurrency from '../../utils/formatCurrency'
+import { Container, CategoryImg, ContainerItems, Image, Button } from './styles'
+
+function OffersCarousel() {
+  const [offers, setOffers] = useState([])
+
+  useEffect(() => {
+    async function loadOffers() {
+      const { data } = await api.get('products')
+
+      const onlyOffers = data
+        .filter(product => product.offer)
+        .map(product => {
+          return { ...product, formatedPrice: formatCurrency(product.price) }
+        })
+
+      setOffers(onlyOffers)
+    }
+
+    loadOffers()
+  }, [])
+
+  const breakPoints = [
+    { width: 1, itemsToShow: 1 },
+    { width: 400, itemsToShow: 2 },
+    { width: 600, itemsToShow: 3 },
+    { width: 900, itemsToShow: 4 },
+    { width: 1300, itemsToShow: 5 }
+  ]
+
+  return (
+    <Container>
+      <CategoryImg src={Offers} alt="logo da Oferta" />
+
+      <Carousel
+        itemsToShow={5}
+        style={{ width: '90%' }}
+        breakPoints={breakPoints}
+      >
+        {offers &&
+          offers.map(product => (
+            <ContainerItems key={product.id}>
+              <Image src={product.url} alt="foto do produto" />
+              <p>{product.name}</p>
+              <p>{product.formatedPrice}</p>
+              <Button>Peça agora</Button>
+            </ContainerItems>
+          ))}
+      </Carousel>
+    </Container>
+  )
+}
+
+export default OffersCarousel
--- src/containers/Home/index.js
+++ src/containers/Home/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 
 import HomeLogo from '../../assets/home-logo.svg'
 import CategoryCarousel from '../../components/CategoryCarousel'
+import OffersCarousel from '../../components/OffersCarousel'
 import { Container, HomeImg } from './styles'
 
 function Home() {
@@ -9,6 +10,7 @@ function Home() {
     <Container>
       <HomeImg src={HomeLogo} alt="logo da home" />
       <CategoryCarousel />
+      <OffersCarousel />
     </Container>
   )
 }
