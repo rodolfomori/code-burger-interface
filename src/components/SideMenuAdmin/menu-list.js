8dbca1cdaf5da0c009705bb73cd3b7a1f0cd0668
Rodolfo Mori <rod_dolpho@hotmail.com>
2022-02-17 08:48:34 -0300

    finished sidemenuadmin

---
+++ src/components/SideMenuAdmin/menu-list.js
@@ -0,0 +1,12 @@
+import ShoppingBagIcon from '@mui/icons-material/ShoppingBag'
+
+const listLinks = [
+  {
+    id: 1,
+    label: 'Pedidos',
+    link: '/pedidos',
+    icon: ShoppingBagIcon
+  }
+]
+
+export default listLinks
--- src/containers/Admin/index.js
+++ src/containers/Admin/index.js
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { SideMenuAdmin } from '../../components'
 import Orders from './Orders'
 import { Container } from './styles'
 
 export function Admin() {
   return (
     <Container>
+      <SideMenuAdmin />
       <Orders />
     </Container>
   )
