b63b33ab70e933bb73330616ec2164c45701c5fc
Rodolfo Mori <rod_dolpho@hotmail.com>
2022-02-18 15:54:00 -0300

    finished class16

--- src/containers/Admin/NewProduct/index.js
+++ src/containers/Admin/NewProduct/index.js
@@ -1,9 +1,15 @@
 import React, { useEffect, useState } from 'react'
+import { useForm } from 'react-hook-form'
+import ReactSelect from 'react-select'
 
+import { Button } from '../../../components'
 import api from '../../../services/api'
-import { Container } from './styles'
+import { Container, Label, Input } from './styles'
 
 function NewProduct() {
+  const { register, handleSubmit } = useForm()
+  const onSubmit = data => console.log(data)
+
   useEffect(() => {
     async function loadOrders() {
       const { data } = await api.get('products')
@@ -13,7 +19,20 @@ function NewProduct() {
 
   return (
     <Container>
-      <div>Ok</div>
+      <form noValidate>
+        <Label>Nome</Label>
+        <Input type="text" {...register('name')} />
+
+        <Label>Preço</Label>
+        <Input type="number" {...register('price')} />
+
+        <Label>Upload da imagem</Label>
+        <Input type="file" accept="image/png, image/jpeg" />
+
+        <ReactSelect />
+
+        <Button>Adicionar produto</Button>
+      </form>
     </Container>
   )
 }
--- src/containers/Admin/NewProduct/styles.js
+++ src/containers/Admin/NewProduct/styles.js
@@ -1,3 +1,7 @@
 import styled from 'styled-components'
 
 export const Container = styled.div``
+
+export const Label = styled.p``
+
+export const Input = styled.input``
